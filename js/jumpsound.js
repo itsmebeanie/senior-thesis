470771673b91c9c7e12fa13abfd31a8481d18458
kaitlingu <kaitlin.gu@gmail.com>
2016-04-13 19:53:13 -0400

    senior

--- js/jumpsound.js
+++ js/jumpsound.js
@@ -203,9 +203,9 @@ var Webgl = (function() {
 /***********************************************************************/
 setup = {
     dodecahedronSize: {
-        treble: 0.05,
-        medium: 0.05,
-        bass: 0.02
+      treble: 0.05,
+      medium: 0.01,
+      bass: 0.01
     },
     /* mario themed colors */
     dodecahedronColor: {
