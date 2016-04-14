3718755d46ef53e634bbe1d6ad83732e39e96b81
kaitlingu <kaitlin.gu@gmail.com>
2016-04-13 20:03:33 -0400

    senior

--- js/jumpsound.js
+++ js/jumpsound.js
@@ -132,7 +132,7 @@ var Webgl = (function() {
         var self = this;
         this.scene = new THREE.Scene();
 
-        this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 800);
+        this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000);
         this.camera.position.z = 800;
 
         this.camera.lookAt(this.scene.position);
